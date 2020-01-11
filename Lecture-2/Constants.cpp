41178413c11e7c9c5a8520251914f0ed1da0bbab
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-01-11 15:25:30 +0530

    Lecture-2

---
+++ Lecture-2/Constants.cpp
@@ -0,0 +1,26 @@
+// Constants
+#include <iostream>
+using namespace std;
+#define PI 3.14+1
+#define SEMICOLON ;
+#define PRINT cout<<
+int main(){
+	// PI = PI +1;
+
+	PRINT PI<<endl SEMICOLON
+	int r;
+	cin>>r;
+	PRINT 2*PI*r<<endl SEMICOLON
+
+
+	// const int area_of_house = 3000;
+
+
+
+	// cout<<area_of_house<<endl;
+	
+	// cout<<area_of_house<<endl;
+
+
+	return 0;
+}
---
+++ Lecture-2/sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 32;
+	float b = 1.11;
+	char ch = 'A';
+	
+	cout<<sizeof('A')<<endl;
+	cout<<sizeof(int)<<endl;
+	cout<<sizeof(ch)<<endl;
+	cout<<sizeof(32)<<endl;
+	
+
+
+	return 0;
+}
