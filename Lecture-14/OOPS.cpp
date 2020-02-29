9475aadf6e37e1a9194bea766e5009bac5085971
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-02-29 14:21:48 +0530

    Lecture-14

--- Lecture-14/OOPS.cpp
+++ Lecture-14/OOPS.cpp
@@ -1,5 +1,6 @@
 // OOPS
 #include<iostream> 
+#include <cstring>
 using namespace std;
 
 /////////////////////// BLUEPRINT //////////////////////////////
@@ -8,6 +9,27 @@ public:
 	char name[20];
 	int price;
 	int model;
+
+	// 1. Default Constructor
+	Car(){
+		cout<<"In Default Constructor"<<endl;
+	}
+
+	// 2. Parameterized Constructor
+	Car(char*n,int p,int m){
+		cout<<"In Parameterized Constructor"<<endl;
+		strcpy(name,n);
+		price = p;
+		model = m;
+	}
+
+	// 3. Copy Constructor
+	Car(Car &X){
+		cout<<"In Copy Constructor"<<endl;
+		strcpy(name,X.name);
+		price = X.price;
+		model = X.model;
+	}
 
 	void Print(){
 		cout<<"Name  : "<<name<<endl;
@@ -34,11 +56,21 @@ int main(){
 	strcpy(B.name,"Audi");
 	B.price = 120;
 	B.model = 2020;
+
+	Car C("Maruti",100,2019);
+	
+	// Calling Copy Constructor
+	Car D = C; // or Car D(C);
+
+
 	// cout<<B.name<<endl;
 	// cout<<B.price<<endl;
 	// cout<<B.model<<endl;
 	A.Print();
 	B.Print();
+	C.Print();
+	D.Print();
+	
 	// A.name[0] = 'B';
 	// A.name[1] = 'M';
 	// A.name[2] = 'W';
