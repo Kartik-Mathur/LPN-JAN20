e362ad006ad08a6c825eeffcf19aad982fa07eec
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-04-05 15:46:37 +0530

    Graphs

--- Lecture-25/GenericGraph.cpp
+++ Lecture-25/GenericGraph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include <vector>
 #include <unordered_map>
 #include <list>
@@ -49,6 +50,46 @@ public:
 		}
 		cout<<endl;
 	}
+
+	int SSSP(T src, T des){
+		unordered_map<T,int> dist;
+		unordered_map<T,T> parent_map;
+		// Set every node distance to infinity
+		for(auto node:adjList){
+			dist[node.first] = INT_MAX;
+		}
+		queue<T> q;
+		dist[src] = 0;
+		parent_map[src] = src;
+		q.push(src);
+
+		while(!q.empty()){
+			T parent = q.front();
+			q.pop();
+			int parent_dist = dist[parent];
+
+			for(auto children:adjList[parent]){
+				if(dist[children] == INT_MAX){
+					parent_map[children] = parent;
+					dist[children] = 1 + parent_dist;
+					q.push(children);
+				}
+			}
+		}
+
+		// for(auto x: dist){
+		// 	cout<<"Distance of "<<x.first<<" from "<<src<<" : "<<x.second<<endl;
+		// }
+		T temp = des;
+
+		while(temp!=src){
+			cout<<temp<<"<--";
+			temp = parent_map[temp];
+		}	
+		cout<<src<<endl;
+
+		return dist[des];
+	}
 };
 
 int main(){
@@ -63,17 +104,35 @@ int main(){
 	// g.addEdge("Putin","Trump",false);
 	// g.addEdge("Putin","Pope",false);
 
-	g.addEdge(0,1);
-	g.addEdge(2,1);
-	g.addEdge(2,4);
-	g.addEdge(3,4);
-	g.addEdge(3,2);
-	g.addEdge(3,5);
-	g.addEdge(0,4);
+	// g.addEdge(0,1);
+	// g.addEdge(2,1);
+	// g.addEdge(2,4);
+	// g.addEdge(3,4);
+	// g.addEdge(3,2);
+	// g.addEdge(3,5);
+	// g.addEdge(0,4);
+	int board[50] = {0};
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[17]=-13;
+	board[20] = -14;
+	board[24] = -8;
+	board[25] = -10;
+	board[32] = -2;
+	board[34] = -22;
+
+	for(int u=0;u<=36;u++){
+		for(int dice = 1;dice<=6;dice++){
+			 int v = u+dice + board[u+dice];
+			 g.addEdge(u,v,false);
+		}
+	}
 
 
-	g.BFS(0);
-
+	// g.BFS(0);
+	cout<<"Min Moves : "<<g.SSSP(0,36)<<endl;
 
 	return 0;
 }
