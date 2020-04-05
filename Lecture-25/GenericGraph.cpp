445477d626ea8c434d353ca8373a95fb2d953f68
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-04-05 15:01:26 +0530

    Lecture-25 Graphs

--- Lecture-25/GenericGraph.cpp
+++ Lecture-25/GenericGraph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <list>
+#include<queue>
 using namespace std;
 
 template<typename T>
@@ -26,20 +27,52 @@ public:
 		}
 		cout<<endl;
 	}
+
+	void BFS(T src){
+		unordered_map<T,bool> visited;
+		queue<T> q;
+
+		visited[src] = true;
+		q.push(src);
+
+		while(!q.empty()){
+			T parent = q.front();
+			cout<<parent<<' ';
+			q.pop();
+
+			for(auto children:adjList[parent]){
+				if(!visited[children]){
+					q.push(children);
+					visited[children] = true;
+				}
+			}
+		}
+		cout<<endl;
+	}
 };
 
 int main(){
-	Graph<string> g;
+	// Graph<string> g;
+	Graph<int> g;
 
-	g.addEdge("Modi","Trump");
-	g.addEdge("Modi","Yogi",false);
-	g.addEdge("Yogi","Prabhu",false);
-	g.addEdge("Prabhu","Modi",false);
-	g.addEdge("Putin","Modi",false);
-	g.addEdge("Putin","Trump",false);
-	g.addEdge("Putin","Pope",false);
+	// g.addEdge("Modi","Trump");
+	// g.addEdge("Modi","Yogi",false);
+	// g.addEdge("Yogi","Prabhu",false);
+	// g.addEdge("Prabhu","Modi",false);
+	// g.addEdge("Putin","Modi",false);
+	// g.addEdge("Putin","Trump",false);
+	// g.addEdge("Putin","Pope",false);
 
-	g.Print();
+	g.addEdge(0,1);
+	g.addEdge(2,1);
+	g.addEdge(2,4);
+	g.addEdge(3,4);
+	g.addEdge(3,2);
+	g.addEdge(3,5);
+	g.addEdge(0,4);
+
+
+	g.BFS(0);
 
 
 	return 0;
